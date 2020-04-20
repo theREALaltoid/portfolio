1c8aa893b8f686d1bf7addfd70c187cbf76d1db4
theREALaltoid <oronazac@protonmail.com>
2020-04-20 15:39:10 -0700

    Medium Updates

---
+++ build/precache-manifest.1834416c46d75e554334cd9603d3a22a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0dc2f5d0392589d9e2e5ef95c61bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a70ad6b4c4961b3f9dc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cbe4f9db291b1fe2c5ed",
+    "url": "/static/css/main.b3ba1845.chunk.css"
+  },
+  {
+    "revision": "1a70ad6b4c4961b3f9dc",
+    "url": "/static/js/2.a91bf2bf.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.a91bf2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe4f9db291b1fe2c5ed",
+    "url": "/static/js/main.79ad45b3.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "9c758a0244069d8e4f24055d01a462aa",
+    "url": "/static/media/backgroundOne.9c758a02.jpg"
+  },
+  {
+    "revision": "8620872d6e1ad707fc54c405e30bf3e6",
+    "url": "/static/media/backgroundTwo.8620872d.jpg"
+  }
+]);
